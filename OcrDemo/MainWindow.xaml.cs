efb7c3f1b27ba4ae6172283812a9c30eeaf20f08
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2019-04-30 22:22:14 +0100

    Add project files.

---
+++ OcrDemo/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OcrDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<TabPlugin> _plugins = new List<TabPlugin>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            Plugins.Add(new TabPlugin("OcrDemo.ScreenGrab.View", "Simple Screen grab"));
+            Plugins.Add(new TabPlugin("OcrDemo.Settings.View", "Settings"));
+        }
+
+        internal List<TabPlugin> Plugins { get => _plugins;  }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadPlugins();
+        }
+
+        private void LoadPlugins()
+        {
+            foreach(var plugin in this.Plugins)
+            {
+                TabItem tItem = new TabItem();
+                Type tyPlugin=Type.GetType(plugin.TypeName, false, false);
+                Object ctl=Activator.CreateInstance(tyPlugin);
+                if (ctl == null)
+                {
+                    MessageBox.Show($" Could not load plugin View:{tItem}");
+                }
+                tItem.Content = ctl;
+                tItem.Header = plugin.TabTitle;
+                ctlTabMain.Items.Add(tItem);
+            }
+        }
+    }
+    /// <summary>
+    /// Represents a Tabbed view which will be added dynamically to the main Tabbed view
+    /// </summary>
+    class TabPlugin
+    {
+        public TabPlugin(string type,string title)
+        {
+            this.TypeName = type;
+            this.TabTitle = title;
+        }
+        /// <summary>
+        /// The class name of the User control
+        /// </summary>
+        public string TypeName { get; set; }
+        /// <summary>
+        /// Title of the Tab view
+        /// </summary>
+        public string TabTitle { get; set; }
+        public override string ToString()
+        {
+            return $"Type={TypeName}    Title={TabTitle}";
+        }
+    }
+}
---
+++ OcrDemo/ScreenGrab/View.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OcrDemo.ScreenGrab
+{
+    /// <summary>
+    /// Interaction logic for View.xaml
+    /// </summary>
+    public partial class View : UserControl
+    {
+        System.Windows.Forms.PictureBox ctlPicBox;
+        public View()
+        {
+            InitializeComponent();
+            ctlPicBox = new System.Windows.Forms.PictureBox();
+            ctlHost.Child = ctlPicBox;
+            this.Loaded += View_Loaded;
+        }
+
+        private void View_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnCopyImage_Click(object sender, RoutedEventArgs e)
+        {
+            var oClip = System.Windows.Forms.Clipboard.GetImage();
+            if (oClip == null) return;
+            ctlPicBox.Image = oClip;
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            ctlPicBox.Image = null;
+        }
+
+        private void BtnOCR_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("OCR - to be done");
+        }
+    }
+}
