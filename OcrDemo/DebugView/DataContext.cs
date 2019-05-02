1467ff2e4ed216244faa7121f53de95d06c52037
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2019-05-02 17:02:41 +0100

    Added DebugView for rapid debugging of UI dialogs and widgets

---
+++ OcrDemo/DebugView/DataContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using OcrDemo.Contracts.interfaces;
+
+namespace OcrDemo.DebugView
+{
+    public class DataContext : INotifyPropertyChanged
+    {
+        public DataContext()
+        {
+            _fileSample = SettingsDevOnly.Default.file_samplepicture;
+        }
+        private string _fileSample;
+        public IMainView Main { get; internal set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public string SampleImage
+        {
+            get { return _fileSample; }
+            set { _fileSample = value; NotifyPropertyChanged(); }
+        }
+        internal void SaveSettings()
+        {
+            SettingsDevOnly.Default.file_samplepicture = this.SampleImage;
+            SettingsDevOnly.Default.Save();
+        }
+    }
+}
---
+++ OcrDemo/DebugView/View.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using OcrDemo.Contracts.interfaces;
+
+namespace OcrDemo.DebugView
+{
+    /// <summary>
+    /// Interaction logic for View.xaml
+    /// </summary>
+#if DEBUG
+        //We want this View while debugging only
+    [Export(typeof(OcrDemo.Contracts.interfaces.IPluginTabbedView))]
+    [ExportMetadata(OcrDemo.Contracts.Constants.MEF_ATTRIBUTE_PLUGINNAME, "debugpanel")]
+    [ExportMetadata(OcrDemo.Contracts.Constants.MEF_ATTRIBUTE_PLUGIN_TAB_TITLE, "Debug Widgets and Dialogs")]
+#endif
+    public partial class View : UserControl, OcrDemo.Contracts.interfaces.IPluginTabbedView
+    {
+        public View()
+        {
+            InitializeComponent();
+            this.Loaded += View_Loaded;
+        }
+        public DataContext ViewModel
+        {
+            get
+            {
+                return this.Resources["theModel"] as DataContext;
+            }
+        }
+
+        public IMainView Main { get ; set ; }
+
+        public void OnActivate()
+        {
+            
+        }
+
+        public void OnInit()
+        {
+            ViewModel.Main = Main;   
+        }
+
+        private void View_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnLaunchResultsViewer_Click(object sender, RoutedEventArgs e)
+        {
+            Main.SetStatus(0, "");
+            if (System.IO.File.Exists(ViewModel.SampleImage)==false)
+            {
+                Main.SetStatus(0,"Please specify a sample image file");
+                return;
+            }
+            OcrDemo.UI.Lib.OcrResults win = new UI.Lib.OcrResults();
+            var bytes = System.IO.File.ReadAllBytes(ViewModel.SampleImage);
+
+            win.RenderImage(bytes, null);
+            win.Show();
+            ViewModel.SaveSettings();
+        }
+    }
+}
